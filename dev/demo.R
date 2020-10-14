cbf1597923c2e9f3964bb5976ca9f8b196153940
Toño <j.diaz@opianalytics.com>
2020-10-14 09:27:21 -0500

    Add demo file

---
+++ dev/demo.R
@@ -0,0 +1,76 @@
+ag=50
+age_dia=48
+woma=1
+eth=0
+smok=0
+sb=127
+hba1=11
+ld=143
+hd=47.73
+weigh=65
+tall=1.60
+
+
+time=20
+complicaciones=8
+
+
+
+#### flag
+
+mii=0
+strok=0
+ih=0
+ch=0
+blin=0
+ampu=0
+rena=0
+ulce=0
+
+
+#history=c(age,age_diab,woman,ethn,smoke,sbp,hba1c,ldl,hdl,weight,talla,age-age_diab,weight/(talla^2),sbp/10,ulcer,mi,stroke,ihd,
+#chf,blind,amput,renal,1,0,0,0,0,0,0,0,0)
+
+len_his=31
+
+
+hba1c_red1_comp1_mean=-1.6
+hba1c_red1_comp1_se=0.2
+
+weight_red1_comp1_mean=2.30
+weight_red1_comp1_se=0.2
+
+sbp_red1_comp1_mean=2.9
+sbp_red1_comp1_se=0.2
+
+ldl_red1_comp1_mean=-6.4
+ldl_red1_comp1_se=0.2
+
+hdl_red1_comp1_mean=0
+hdl_red1_comp1_se=0.2
+
+sbp_red1_comp1_mean=sbp_red1_comp1_mean/10
+sbp_red1_comp1_se=sbp_red1_comp1_se/100
+
+stan_data=list(ag=ag,age_dia=age_dia,woma=woma,eth=eth,smok=smok,sb=sb,hba1=hba1,ld=ld,hd=hd,
+               weigh=weigh,tall=tall,ulce=ulce,mii=mii,strok=strok,ih=ih,ch=ch,blin=blin,
+               ampu=ampu,rena=rena,time=time,complicaciones=complicaciones,hba1c_red1_comp1_mean=hba1c_red1_comp1_mean,
+               hba1c_red1_comp1_se=hba1c_red1_comp1_se,weight_red1_comp1_mean=weight_red1_comp1_mean,weight_red1_comp1_se=weight_red1_comp1_se,
+               sbp_red1_comp1_mean=sbp_red1_comp1_mean,sbp_red1_comp1_se=sbp_red1_comp1_se,ldl_red1_comp1_mean=ldl_red1_comp1_mean,ldl_red1_comp1_se=ldl_red1_comp1_se,
+               hdl_red1_comp1_mean=hdl_red1_comp1_mean,hdl_red1_comp1_se=hdl_red1_comp1_se,len_his=len_his)
+
+library(rstan)
+options(mc.cores = 4)
+rstan_options(auto_write = TRUE)
+
+library(simDiabetesStan)
+
+m=diabetes_stan()
+fit <- sampling(m, data=stan_data,
+                iter=10000, warmup=0, chains=1,
+                algorithm="Fixed_param")
+
+
+
+stan_fit <- extract(fit)
+
